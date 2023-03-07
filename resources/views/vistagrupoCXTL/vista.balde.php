28a4d6b5d993ff5ab6eb2e22510e751c1a79f3e8
unknown <SENA@SOAAPRCDIFSD062.FormacionSena.RED>
2023-03-07 14:34:07 -0500

    grupo XCLT

---
+++ resources/views/vistagrupoCXTL/vista.balde.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style> 
+  @import url('https://fonts.googleapis.com/css2?family=Caveat&display=swap');
+  
+  </style> <link rel="stylesheet" href="..\resources\views\vistagrupoCXTL\estilos1.css">
+</head>
+<body>
+  
+
+
+<div class="landing--container">
+<div class="landing--title">
+    <b>Bienvenido al noticiero minimalista</b>
+    <h1>Donde menos, es mas</h1>
+    <a href="php5\verifica_ident.php">
+    <button class="primary-btn">Administrativo</button></a>
+    <a href="primero.php">
+    <button class="primary-btn1">Lector</button></a>
+    
+  </div>
+
+</div>
+
+
+<section class="color-bg">
+<h2>...Ingresa como...</h2>
+
+</section>
+
+
+<footer>
+    <p class="footer--text">© <p>&copy Proyecto Desarrollado por el Grupo de ADSI TATIANA MARTINEZ <?php echo date("Y");?></p>
+  </div>
+</footer>
+
+</body>
+</html>
+
+</body>
+</html>
