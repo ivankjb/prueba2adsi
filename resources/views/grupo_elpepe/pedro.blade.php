17c2d8213ca2a65e4737dcdcc6e950edb0f03a76
unknown <SENA@SOAAPRCDIFSD074.FormacionSena.RED>
2023-03-07 16:55:17 -0500

    ejemplo1

---
+++ resources/views/grupo_elpepe/pedro.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        body{
+            background-color: #797d7f;
+            color:aliceblue;
+        }
+    </style>
+    <title>Archivo 2</title>
+</head>
+<body>
+    <h1>es un ejemplo 2</h1>
+</body>
+</html>
+
---
+++ resources/views/grupo_elpepe/pepito.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Archivo</title>
+    <style>
+    body{
+            background-color: #fff;
+            color: #797d7f;
+        }
+        </style>
+</head>
+<body>
+    <h1>ejemplo 2</h1>
+</body>
+</html>
